926040998c88ffde76cd6a87deee5ae1276d05b7
louisbl <louis@beltramo.me>
2015-01-21 16:17:25 +0100

    Display HomeActivity after Splashscreen

--- app/src/main/java/crm/gobelins/materialplaylist/splash/SplashActivity.java
+++ app/src/main/java/crm/gobelins/materialplaylist/splash/SplashActivity.java
@@ -1,17 +1,41 @@
 package crm.gobelins.materialplaylist.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 
 import crm.gobelins.materialplaylist.R;
+import crm.gobelins.materialplaylist.home.HomeActivity;
 
 
 public class SplashActivity extends FragmentActivity {
+
+    private static final int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+
+                // start Home Activity
+                startActivity(i);
+
+                // close the splashscreen
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
