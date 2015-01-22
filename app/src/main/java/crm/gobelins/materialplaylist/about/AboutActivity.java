3156bc759e433d582fa38a0d1f4803c644ab0569
louisbl <louis@beltramo.me>
2015-01-22 19:18:23 +0100

    Add support for up navigation on API level > 16

--- app/src/main/java/crm/gobelins/materialplaylist/about/AboutActivity.java
+++ app/src/main/java/crm/gobelins/materialplaylist/about/AboutActivity.java
@@ -1,7 +1,9 @@
 package crm.gobelins.materialplaylist.about;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 
 import crm.gobelins.materialplaylist.R;
 import crm.gobelins.materialplaylist.appinfo.AppInfoFragment;
@@ -28,5 +30,15 @@ public class AboutActivity extends ActionBarActivity {
                     .add(R.id.app_info_container, new AppInfoFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
